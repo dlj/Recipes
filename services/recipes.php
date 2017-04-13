e0dcf86e86a5b49b01c2ab330d1ce293b1514a93
Daniel Valsø Lykkegaard <biohead@gmail.com>
2017-04-13 16:17:56 +0200

    Changed my mind... going with plural

---
+++ services/recipes.php
@@ -0,0 +1,47 @@
+<?php
+require("service.php");
+
+class recipe extends service
+{
+
+    public $objectDefinitionType = "recipeObject";
+
+    function __construct()
+    {
+        $this->table = 'recipe';
+        parent::__construct();
+    }
+    
+  /* Comment this in again, if overriding of default service code is needed
+  
+    public function get(objectDefinition $recipeObject = null)
+    {
+       return parent::get($recipeObject);
+    }
+
+    public function post(objectDefinition $recipeObject)
+    {
+      return parent::post($recipeObject);
+    }
+  
+    public function put(objectDefinition $recipeObject)
+    {
+      return parent::put($recipeObject);
+    }
+  
+    public function delete(objectDefinition $recipeObject)
+    {
+      return parent::delete($recipeObject);
+    }
+*/
+
+}
+
+
+class recipeObject extends objectDefinition
+{
+    public $id;
+    public $name;
+    public $description;
+    public $difficulty;
+}
